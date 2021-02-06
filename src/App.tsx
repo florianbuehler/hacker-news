5080f82765d775774372d32aeddde3c4ea3933f2
florian-buehler@outlook.com <Cal!sthen!c5>
2021-02-06 19:53:25 +0100

    chore: Add react router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,28 @@
-import React, { Component } from 'react'
-import InfiniteScroll from 'react-infinite-scroll-component'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import TopStories from './components/Views/TopStories'
 
-type Props = {
-  storyIds: number[]
-  page: number
-  isFetching: boolean
-}
-
-class App extends Component<Props> {
-  componentDidMount(): void {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    this.props.fetchStoriesFirstPage()
-  }
-
-  fetchStories = (): void => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const { storyIds, page, fetchStories, isFetching } = this.props
-    if (!isFetching) {
-      fetchStories({ storyIds, page })
-    }
-  }
-
-  render(): React.ReactElement {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const { stories, hasMoreStories } = this.props
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return (
-      <div>
-        <h1>Hacker News</h1>
-        <InfiniteScroll
-          next={this.fetchStories}
-          hasMore={hasMoreStories}
-          loader={<h3>loading...</h3>}
-          dataLength={stories.length}
-        >
-          <ul>
-            {stories.map(
-              (story: never): React.ReactElement => (
-                // eslint-disable-next-line react/jsx-key
-                <li>
-                  <h2>hello</h2>
-                </li>
-              )
-            )}
-          </ul>
-        </InfiniteScroll>
-      </div>
-    )
-  }
+const App: React.FC = () => {
+  return (
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/stories/best" />
+        </Route>
+        <Route exact path="/stories/best">
+          <TopStories />
+        </Route>
+        <Route exact path="/stories/top">
+          <TopStories />
+        </Route>
+        <Route exact path="/stories/new">
+          <TopStories />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/components/Views/TopStories/TopStories.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+type Props = {
+  storyIds: number[]
+  stories: any
+  page: number
+  hasMoreStories: boolean
+  fetchStoriesFirstPage: () => void
+  fetchStories: (storyIds: number[], page: number) => any
+  isFetching: boolean
+}
+
+const TopStories: React.FC<Props> = (props) => {
+  useEffect(() => {
+    props.fetchStoriesFirstPage()
+  }, [])
+
+  const fetchStories = (): void => {
+    const { storyIds, page, fetchStories, isFetching } = props
+    if (!isFetching) {
+      fetchStories(storyIds, page)
+    }
+  }
+
+  const { stories, hasMoreStories } = props
+
+  return (
+    <div>
+      <h1>Hacker News</h1>
+      <InfiniteScroll
+        next={fetchStories}
+        hasMore={hasMoreStories}
+        loader={<h3>loading...</h3>}
+        dataLength={stories.length}
+      >
+        <ul>
+          {stories.map(
+            (story: never): React.ReactElement => (
+              // eslint-disable-next-line react/jsx-key
+              <li>
+                <h2>hello</h2>
+              </li>
+            )
+          )}
+        </ul>
+      </InfiniteScroll>
+    </div>
+  )
+}
+
+export default TopStories
