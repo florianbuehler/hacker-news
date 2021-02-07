c4ca954a6b2d9005c2bdc577c83fbe1894e2466b
Florian Bühler <florian-buehler@outlook.com>
2021-02-07 11:24:56 +0100

    refactor: Improve structure of the app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import routes from './routes'
-import TopStories from 'components/Views/TopStories'
+import PageLayout from 'components/layouts/PageLayout'
+import TopStories from 'components/views/TopStories'
 
 const App: React.FC = () => {
   return (
-    <Router>
-      {/* A <Switch> looks through its children <Route>s and
+    <PageLayout>
+      <Router>
+        {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
-      <Switch>
-        <Route exact path={routes.home}>
-          <Redirect to={routes.stories.best} />
-        </Route>
-        <Route exact path={routes.stories.best}>
-          <TopStories />
-        </Route>
-        <Route exact path={routes.stories.top}>
-          <TopStories />
-        </Route>
-        <Route exact path={routes.stories.new}>
-          <TopStories />
-        </Route>
-        <Route path="*">
-          <h1>No match</h1>
-        </Route>
-      </Switch>
-    </Router>
+        <Switch>
+          <Route exact path={routes.home}>
+            <Redirect to={routes.stories.best} />
+          </Route>
+          <Route exact path={routes.stories.best}>
+            <TopStories />
+          </Route>
+          <Route exact path={routes.stories.top}>
+            <TopStories />
+          </Route>
+          <Route exact path={routes.stories.new}>
+            <TopStories />
+          </Route>
+          <Route path="*">
+            <h1>No match</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </PageLayout>
   )
 }
 
