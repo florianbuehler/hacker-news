1d06d01076f648248876eab41fbbdb3a8ab98989
Florian Bühler <florian-buehler@outlook.com>
2021-02-08 19:08:29 +0100

    feat: Add tailwind colors and improve header styling.

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -4,7 +4,7 @@ import routes from 'routes'
 
 const Header: React.FC = (): React.ReactElement => {
   return (
-    <header className="px-12 py-12 flex">
+    <header className="px-12 py-12 bg-grey-800 flex">
       <Link to={routes.home}>
         <h1 className="text-5xl">Hacker News</h1>
       </Link>
