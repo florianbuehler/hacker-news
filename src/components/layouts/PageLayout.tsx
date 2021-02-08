022e61a7f78b8e6152b59a45e1b78361fab22929
Florian Bühler <florian-buehler@outlook.com>
2021-02-08 20:21:19 +0100

    chore: Configure tailwind for dark mode setup.

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import classnames from 'classnames'
 import Header from 'components/molecules/Header'
 import NavBar from 'components/molecules/Navbar'
 
 const PageLayout: React.FC = ({ children }): React.ReactElement => {
+  const darkMode = false
+
   return (
-    <>
+    <div className={classnames(darkMode ? 'dark' : '', 'flex flex-col min-h-screen')}>
       <Header />
       <NavBar />
-      <main className="max-w-5xl mx-auto bg-blue-500">{children}</main>
-    </>
+      <main className="bg-grey-50 flex-1 dark:bg-grey-900">
+        <div className="max-w-5xl mx-auto">{children}</div>
+      </main>
+    </div>
   )
 }
 
