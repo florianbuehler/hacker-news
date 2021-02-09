b4e1936d836fd4edef8a4bbfddfd2e2d15a60f40
Florian Bühler <florian-buehler@outlook.com>
2021-02-09 20:32:06 +0100

    feat: Add button to toggle dark mode.

--- src/components/molecules/Navbar.tsx
+++ src/components/molecules/Navbar.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import classnames from 'classnames'
 import routes from 'routes'
 import NavLink from 'components/atoms/NavLink'
 
-const NavBar: React.FC = (): React.ReactElement => {
+type Props = {
+  isDarkMode: boolean
+  toggleDarkMode: (isDarkMode: boolean) => void
+}
+
+const NavBar: React.FC<Props> = ({ isDarkMode, toggleDarkMode }): React.ReactElement => {
+  const toggleClasses = classnames('bg-white w-4 h-4 rounded-full shadow-md', isDarkMode ? 'ml-auto' : 'mr-auto')
+
   return (
-    <div className="px-12 py-2 bg-grey-50 border-b-2 border-grey-200 flex justify-between dark:bg-grey-900">
+    <div className="px-12 py-2 bg-grey-50 border-b-2 border-grey-200 flex justify-between items-center transition-colors dark:bg-grey-900">
       <nav>
         <ul className="flex flex-row">
           <li>
@@ -18,8 +26,11 @@ const NavBar: React.FC = (): React.ReactElement => {
           </li>
         </ul>
       </nav>
-      <div className="w-10 h-6 flex items-center bg-gray-300 rounded-full p-1">
-        <div className="bg-white w-4 h-4 rounded-full shadow-md" />
+      <div
+        className="w-10 h-6 flex items-center bg-gray-300 rounded-full p-1"
+        onClick={() => toggleDarkMode(!isDarkMode)}
+      >
+        <div className={toggleClasses} />
       </div>
     </div>
   )
