5182aac319e18dd7b515e10b3879b36c77e0f0a8
Florian Bühler <florian-buehler@outlook.com>
2021-02-11 20:02:32 +0100

    feat: Add time when story was posted.

--- src/components/atoms/StoryCard.tsx
+++ src/components/atoms/StoryCard.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import { Story } from 'store/story/story'
 
-const StoryCard: React.FC<Story> = ({ title, url, score, by, ...props }): React.ReactElement => {
+const getTimePassed = (time: number): string => {
+  const hoursPassed = new Date().getHours() - new Date(time * 1000).getHours()
+
+  console.log(new Date().toISOString(), new Date(time * 1000).toISOString())
+
+  if (hoursPassed > 0) {
+    return hoursPassed === 1 ? `${hoursPassed} hour` : `${hoursPassed} hours`
+  } else {
+    const minutesPassed = new Date().getMinutes() - new Date(time * 1000).getMinutes()
+    return minutesPassed === 1 ? `${minutesPassed} minute` : `${minutesPassed} minutes`
+  }
+}
+
+const StoryCard: React.FC<Story> = ({ title, url, score, by, time, ...props }): React.ReactElement => {
+  const timePassed = getTimePassed(time)
+
   return (
-    <article className="flex flex-row p-4 border-2 rounded-xl shadow border-grey-400 bg-grey-100">
+    <article className="flex flex-row p-4 border rounded-xl shadow border-grey-400 bg-grey-100">
       <div className="flex items-center w-8 justify-center">{score}</div>
       <div className="pl-4">
         <h3 className="text-xl">{title}</h3>
         <p>
           (<a href={url}>{url})</a>
         </p>
-        <p>by {by}</p>
+        <p>
+          by {by} {timePassed} ago
+        </p>
       </div>
     </article>
   )
