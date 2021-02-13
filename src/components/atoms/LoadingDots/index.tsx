6011845b47a1552e0858aef5b5b70cdbd4c539ef
Florian Bühler <florian-buehler@outlook.com>
2021-02-13 19:02:04 +0100

    chore: Use scss instead of css.

--- src/components/atoms/LoadingDots/index.tsx
+++ src/components/atoms/LoadingDots/index.tsx
@@ -2,18 +2,16 @@ import React from 'react'
 import classnames from 'classnames'
 
 // styles
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import './styles.css'
+import classes from './styles.module.scss'
 
 const LoadingDots: React.FC = (): React.ReactElement => {
   const basicClasses = 'bg-grey-500 w-3.5 h-3.5 rounded-full'
 
   return (
     <div className="flex flex-row w-28 justify-between">
-      <span className={classnames(basicClasses, 'firstDot')} />
-      <span className={classnames(basicClasses, 'secondDot')} />
-      <span className={classnames(basicClasses, 'thirdDot')} />
+      <span className={classnames(basicClasses, classes.firstDot)} />
+      <span className={classnames(basicClasses, classes.secondDot)} />
+      <span className={classnames(basicClasses, classes.thirdDot)} />
     </div>
   )
 }
