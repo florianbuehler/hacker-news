7396ca25847d29a7a0cd5f6bd99b9841322e95d4
Florian Bühler <florian-buehler@outlook.com>
2021-02-20 16:31:39 +0100

    feat: Include origin link of story page.

--- src/components/atoms/StoryCard.tsx
+++ src/components/atoms/StoryCard.tsx
@@ -4,7 +4,7 @@ import routes from 'routes'
 import { reverse } from 'named-urls'
 import { Story } from 'store/story/story'
 import { ReactComponent as Arrow } from 'assets/icons/arrow_forward_ios-black-18dp.svg'
-import getSiteHostName from 'utils/getSiteHostName'
+import { getHostName, getOrigin } from 'utils/urlUtils'
 
 const getTimePassed = (time: number): string => {
   const hoursPassed = new Date().getHours() - new Date(time * 1000).getHours()
@@ -18,20 +18,25 @@ const getTimePassed = (time: number): string => {
 }
 
 const StoryCard: React.FC<Story> = ({ title, url, score, by, time, ...props }): React.ReactElement => {
-  const hostName = getSiteHostName(url)
+  const hostName = getHostName(url)
+  const origin = getOrigin(url)
   const timePassed = getTimePassed(time)
 
   return (
     <article className="flex flex-row p-4 border rounded-xl shadow border-grey-300 bg-grey-50">
       <div className="flex items-center w-8 justify-center">{score}</div>
       <div className="pl-5">
-        <h3 className="text-xl">
-          <a href={url} target="_blank">
+        <h3 className="text-xl transition-colors duration-200 hover:text-teal-500">
+          <a href={url} target="_blank" rel="noopener noreferrer">
             {title}
           </a>
         </h3>
         <p className="text-sm">
-          (on <a href={url}>{hostName}</a>)
+          (on{' '}
+          <a href={origin} target="_blank" rel="noopener noreferrer">
+            {hostName}
+          </a>
+          )
         </p>
         <p className="pt-3 text-sm">
           by {by} {timePassed} ago
@@ -39,7 +44,7 @@ const StoryCard: React.FC<Story> = ({ title, url, score, by, time, ...props }): 
       </div>
       <div className="ml-auto flex justify-center items-center">
         <Link to={reverse(routes.stories.details.show, { storyId: props.id })}>
-          <Arrow className="text-grey-600 fill-current object-cover h-6 w-auto" />
+          <Arrow className="text-grey-600 transition-colors duration-200 fill-current object-cover h-6 w-auto hover:text-teal-500" />
         </Link>
       </div>
     </article>
